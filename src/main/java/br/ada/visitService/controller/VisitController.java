a07501023e08bf4d2b67af2bc99e5b360a0f58ef
karenCLima <lima.kcls@gmail.com>
2023-11-23 18:46:08 -0300

    feat/GET-ListarVisitas and Fix/Swagger

--- src/main/java/br/ada/visitService/controller/VisitController.java
+++ src/main/java/br/ada/visitService/controller/VisitController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/visit")
@@ -27,5 +28,10 @@ public class VisitController {
     @GetMapping("/id/{id}")
     public ResponseEntity<Visit> getVisitById(@PathVariable Long id){
         return ResponseEntity.ok(visitService.findVisitById(id));
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<VisitResponse>> getAllVisits(){
+    	return ResponseEntity.ok(visitService.getAllVisits());
     }
 }
