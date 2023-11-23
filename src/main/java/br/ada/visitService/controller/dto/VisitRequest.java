b522b34ea11070e12340d7990afb90d01c3b9823
Raquel Carvalho <raquel.cpc90@gmail.com>
2023-11-23 13:12:05 -0300

    feat: altera-VisitRequest

--- src/main/java/br/ada/visitService/controller/dto/VisitRequest.java
+++ src/main/java/br/ada/visitService/controller/dto/VisitRequest.java
@@ -3,10 +3,13 @@ package br.ada.visitService.controller.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.time.LocalDate;
+
 @AllArgsConstructor
 @Getter
 public class VisitRequest {
     private String userId;
     private boolean newUser;
+    private LocalDate visitDate;
     
 }
