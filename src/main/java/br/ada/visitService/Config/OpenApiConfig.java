0b5e4d3f00ddb8f3dfd886c4c817fa4560d8104e
karenCLima <lima.kcls@gmail.com>
2023-11-23 00:08:11 -0300

    feat/adding-swagger-openApi

--- src/main/java/br/ada/visitService/Config/OpenApiConfig.java
+++ src/main/java/br/ada/visitService/Config/OpenApiConfig.java
@@ -1,5 +1,23 @@
 package br.ada.visitService.Config;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
 public class OpenApiConfig {
+	
+	@Bean
+	public OpenAPI defaultOpenApiConfig() {
+		return new OpenAPI()
+                .info(new Info().title("Visit Service")
+                        .description("Visit Service Request ")
+                        .version("0.0.1"));
+    }
+	
+
 
 }
