a6528a0302dc8d417c4082c33cfb9b93da5e4568
Raquel Carvalho <raquel.cpc90@gmail.com>
2023-11-24 14:37:03 -0300

    feat: get-visita-por-usuario

--- src/main/java/br/ada/visitService/repository/VisitRepository.java
+++ src/main/java/br/ada/visitService/repository/VisitRepository.java
@@ -4,7 +4,12 @@ import br.ada.visitService.model.Visit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface VisitRepository extends JpaRepository<Visit, Long> {
     @Query(value = "SELECT * FROM VISITS WHERE VISIT_ID = :visitId", nativeQuery = true)
     Visit findVisitById(String visitId);
+
+    @Query(value = "SELECT * FROM VISITS WHERE USER_ID = :userId", nativeQuery = true)
+    List<Visit> findVisitsByUserId(String userId);
 }
--- src/main/java/br/ada/visitService/service/VisitService.java
+++ src/main/java/br/ada/visitService/service/VisitService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
@@ -68,4 +69,11 @@ public class VisitService {
 	    visit.setTechnician(technician);
     	return VisitConvert.toResponse(visitRepository.save(visit));
     }
+
+    public List<VisitResponse> getVisitsByUserId(String userId){
+        List<Visit> visits = visitRepository.findVisitsByUserId(userId);
+        return VisitConvert.toResponseList(visits);
+    }
+
+
 }
