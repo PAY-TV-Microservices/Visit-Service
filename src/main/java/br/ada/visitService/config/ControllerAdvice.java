48cde9c3d67b6f145b13bee967d3468a1ebdbc86
Raquel Carvalho <raquel.cpc90@gmail.com>
2023-11-24 21:55:42 -0300

    feat: ajuste-retorno-pendencia

--- src/main/java/br/ada/visitService/config/ControllerAdvice.java
+++ src/main/java/br/ada/visitService/config/ControllerAdvice.java
@@ -1,8 +1,11 @@
 package br.ada.visitService.config;
 
+import br.ada.visitService.exception.PendingPaymentsException;
+import br.ada.visitService.exception.UserNewException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -21,5 +24,19 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
         defaultResponse.setMessage(idNotFoundException.getMessage());
         return new ResponseEntity<>(defaultResponse, notFound);
     }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(UserNewException.class)
+    public String handlerUserNewException(UserNewException exception){
+        return exception.getMessage();
+    }
+
+    @ResponseStatus(code = HttpStatus.FORBIDDEN)
+    @ExceptionHandler(PendingPaymentsException.class)
+    public String handlerPendingPaymentsException(PendingPaymentsException exception){
+        return exception.getMessage();
+    }
+
+
 
 }
